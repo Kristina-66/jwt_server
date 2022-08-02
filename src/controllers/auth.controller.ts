27823a9b91109f7c58890f408b8e93562934898f
Kristina <dead.girl.2012@mail.ru>
2022-08-02 14:08:02 +0400

    add lax

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -13,7 +13,7 @@ const accessTokenCookieOptions: CookieOptions = {
   ),
   maxAge: config.get<number>('accessTokenExpiresIn') * 60 * 1000,
   httpOnly: true,
-  "SameSite=Lax",
+  SameSite: "lax",
 };
 
 // Only set secure to true in production
